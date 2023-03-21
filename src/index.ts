397b527210d446db97c719ba012e48648a0aa79e
Kite <ixkaito@gmail.com>
2023-03-21 14:36:48 +0900

    Add jest

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { writeFileSync, readFileSync } from 'fs';
 import { globSync } from 'glob';
 import path from 'path';
 
+export function isZero(value: number): boolean {
+  return value === 0;
+}
+
 function relativeLinks({ config }: { config?: AstroConfig }): AstroIntegration {
   const base = config?.base
     ? config.base.replace(/^\/*([^\/]+)(.*)([^\/]+)\/*$/, '/$1$2$3')
